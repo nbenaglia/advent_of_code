b929c1b062077b8103034e28090142082f7e508b
Nicola Benaglia <nico.benaz@gmail.com>
2021-12-06 16:46:48 +0100

    Part b

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -20,8 +20,33 @@ fn count_increments(values: &[i64]) -> i32 {
     return increments;
 }
 
+fn count_increments_three_measurement_sliding_window(values: &[i64]) -> i32 {
+    const SLIDING_WINDOW: i32 = 3;
+    let mut window: [i64; 3] = values[0..SLIDING_WINDOW];
+    let window_sum = &values[0] + &values[1] + &values[2];
+
+    let mut increments = 0;
+    
+    for read in &values[3..] {
+        let counter = 0;
+        let current_window =
+        if counter == 3 {
+            if read > &window_sum {
+                increments += 1;
+            };
+            window = *read;
+        }
+    }
+    
+    return increments;
+}
+
 fn main() -> Result<(), Error> {
     let data = read(File::open("input.txt")?)?;
     println!("{}", count_increments(&data));
+    println!(
+        "{}",
+        count_increments_three_measurement_sliding_window(&data)
+    );
     Ok(())
 }
