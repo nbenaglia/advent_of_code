94bf6ff5f7b4281abcda5df1fe8d7aec4d05c79f
Nicola Benaglia <nico.benaz@gmail.com>
2021-12-07 20:25:08 +0100

    WIP day 3

---
+++ 2021/day3/part1.rs
@@ -0,0 +1,13 @@
+use std::fs;
+
+fn main() {
+  let input = fs::read_to_string("input.txt").expect("Unable to reafd file");
+  
+  let gamme_rate: i32;
+  let epsilon_rate: i32;
+
+  for line in input.lines() {
+  }
+
+  println!("Power consumption {:?}", decimal_gamme_rate * decimal_epsilon_rate);
+}
