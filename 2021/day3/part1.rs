bade6953e1bd9d6f001bef5fa9d2d9db00582114
Nicola Benaglia <nico.benaz@gmail.com>
2021-12-08 20:19:53 +0100

    Day3 solution

--- 2021/day3/part1.rs
+++ 2021/day3/part1.rs
@@ -1,13 +1,45 @@
-use std::fs;
+static INPUT_FILE: &'static str = include_str!("input.txt");
+
+fn get_decimal_numbers() -> Vec<u16> {
+    return INPUT_FILE
+        .split_ascii_whitespace()
+        .map(|str| u16::from_str_radix(str, 2).unwrap())
+        .collect::<Vec<u16>>();
+}
+
+fn bit_matching(number: u16, position: usize, expected_bit: u16) -> bool {
+    let mask = 1 << position;
+    return ((number & mask) >> position) == (expected_bit as u16);
+}
 
 fn main() {
-  let input = fs::read_to_string("input.txt").expect("Unable to reafd file");
-  
-  let gamme_rate: i32;
-  let epsilon_rate: i32;
+    let numbers = get_decimal_numbers();
+    let mask: u16 = 0x0FFF;
+    let half_total = numbers.len() / 2;
+    let mut counts: [usize; 12] = [0; 12];
 
-  for line in input.lines() {
-  }
+    for number in numbers {
+        for position in 0..12 {
+            if bit_matching(number, position, 1) {
+                counts[position] += 1;
+            }
+        }
+    }
 
-  println!("Power consumption {:?}", decimal_gamme_rate * decimal_epsilon_rate);
+    // assemble back to a number
+    let gamma: u16 = counts
+        .iter()
+        .enumerate()
+        .fold(0, |accum, (position, count)| {
+            let most_common_bit: u16 = if count > &half_total { 1 } else { 0 };
+            return accum | (most_common_bit << position);
+        });
+
+    let epsilon = !gamma & mask;
+    let multiplied = gamma as u64 * epsilon as u64;
+
+    println!(
+        "[Part 1] Gamma: {}, Epsilon: {}, Multiplied: {}",
+        gamma, epsilon, multiplied
+    );
 }
