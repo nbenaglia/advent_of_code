bf621554a6ef3a1020a238606a04832c6424cc18
Nicola Benaglia <nico.benaz@gmail.com>
2021-12-12 18:22:08 +0100

    Get numbers

--- 2021/day4/src/main.rs
+++ 2021/day4/src/main.rs
@@ -1,3 +1,19 @@
+use core::panic;
+
+static INPUT_FILE: &'static str = include_str!("input.txt");
+
+fn get_numbers() -> Vec<u8> {
+    return INPUT_FILE
+        .lines()
+        .nth(0)
+        .unwrap()
+        .split(",")
+        .map(|number| number.parse().unwrap())
+        .collect();
+}
+
+fn get_boards() {}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", get_numbers());
 }
