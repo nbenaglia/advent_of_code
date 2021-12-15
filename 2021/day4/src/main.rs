a74b2b3f2e1fcdd1bc1b1854e6960875df83b09d
Nicola Benaglia <nico.benaz@gmail.com>
2021-12-15 08:27:42 +0100

    WIP

--- 2021/day4/src/main.rs
+++ 2021/day4/src/main.rs
@@ -1,6 +1,16 @@
 use core::panic;
+use ndarray::prelude::*;
 
 static INPUT_FILE: &'static str = include_str!("input.txt");
+
+struct bingo_number {
+    number: u16,
+    marked: bool
+}
+
+struct bingo_card {
+    card: Array5<>
+}
 
 fn get_numbers() -> Vec<u8> {
     return INPUT_FILE
@@ -12,7 +22,15 @@ fn get_numbers() -> Vec<u8> {
         .collect();
 }
 
-fn get_boards() {}
+fn get_boards() {
+    return INPUT_FILE
+    .lines()
+    .nth(2)
+    .unwrap()
+    .split_ascii_whitespace()
+    .map(|number| number.parse().unwrap())
+    .collect();
+}
 
 fn main() {
     println!("{:?}", get_numbers());
